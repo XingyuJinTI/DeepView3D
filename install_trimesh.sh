6950bd375e3a051d30a5d1439bc2c38f5436cb11
mekb <s1794003@sms.ed.ac.uk>
2019-02-17 18:56:31 +0000

    update trimesh

--- install_trimesh.sh
+++ install_trimesh.sh
@@ -1,5 +1,6 @@
-source activate shaperecon
-conda config --add channels conda-forge
-conda install shapely rtree pyembree numpy scipy
-conda install -c conda-forge scikit-image
+export STUDENT_ID=$(whoami)
+source /home/${STUDENT_ID}/miniconda3/bin/activate shaperecon
+/home/${STUDENT_ID}/miniconda3/bin/conda config --add channels conda-forge
+/home/${STUDENT_ID}/miniconda3/bin/conda install shapely rtree pyembree numpy scipy
+/home/${STUDENT_ID}/miniconda3/bin/conda install -c conda-forge scikit-image
 pip install trimesh[all]
