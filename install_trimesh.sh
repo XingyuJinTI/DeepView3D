ed10e1277547036b4095cbb4f5507d03824618d5
Mek Bhumiwat <s1794003@sms.ed.ac.uk>
2019-02-17 21:48:54 +0000

    update validation script

--- install_trimesh.sh
+++ install_trimesh.sh
@@ -1,6 +1,5 @@
-export STUDENT_ID=$(whoami)
-source /home/${STUDENT_ID}/miniconda3/bin/activate shaperecon
-/home/${STUDENT_ID}/miniconda3/bin/conda config --add channels conda-forge
-/home/${STUDENT_ID}/miniconda3/bin/conda install shapely rtree pyembree numpy scipy
-/home/${STUDENT_ID}/miniconda3/bin/conda install -c conda-forge scikit-image
+source activate shaperecon
+conda config --add channels conda-forge
+conda install shapely rtree pyembree numpy scipy
+conda install -c conda-forge scikit-image
 pip install trimesh[all]
