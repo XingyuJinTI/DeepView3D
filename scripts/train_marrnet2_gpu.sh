d03fe1d3b8cc9c07069e3f90044688a31cedc77c
Mek Bhumiwat <s1794003@sms.ed.ac.uk>
2019-02-21 12:20:05 +0000

    use 2 gpu

--- scripts/train_marrnet2_gpu.sh
+++ scripts/train_marrnet2_gpu.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1      # nodes requested
 #SBATCH -n 1      # tasks requested
 #SBATCH --partition=LongJobs
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:2
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-72:00:00
 
